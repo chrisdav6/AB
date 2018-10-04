95f3f9fd7c00d365cb25583fadab552225d51bc2
Chris Davis <chris.davis5440@gmail.com>
2018-10-04 11:11:10 -0400

    created productInfo section on products page

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -10,5 +10,25 @@ $(() => {
     interval: 20000
   });
 
+  //Enable Bootstrap tooltips
+  $('[data-toggle="tooltip"]').tooltip();
+
+  //Load Youtube Videos on Click
+  $(".youtubeEmbed").each(function () {
+    // Set the BG image from the youtube ID
+    $(this).css('background-image', 'url(http://i.ytimg.com/vi/' + this.id + '/hqdefault.jpg)');
+    // Click the video div
+    $(document).delegate('#' + this.id, 'click', function () {
+      // Build embed URL
+      var iframe_url = "http://www.youtube.com/embed/" + this.id + "?autoplay=1&autohide=1";
+      // Grab extra parameters set on div
+      if ($(this).data('params')) iframe_url += '&' + $(this).data('params');
+      // Build iframe tag
+      var iframe = $('<iframe/>', { 'allowfullscreen': 'allowfullscreen', 'frameborder': '0', 'src': iframe_url })
+      // Replace the YouTube thumbnail with YouTube HTML5 Player
+      $(this).append(iframe);
+    });// /click
+  }); // /each video
+
 });
 
