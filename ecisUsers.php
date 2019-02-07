f516e22063549137257575efec0db2468ba8f809
Chris Davis <chris.davis5440@gmail.com>
2019-02-07 14:16:14 -0500

    started building ecisUsers page

--- ecisUsers.php
+++ ecisUsers.php
@@ -29,7 +29,7 @@ $metaDesc = "Label-free Real-time Automated Cell-based assays powered by the tec
     <?php include_once "includes/navbar.php";?>
     <?php include_once "includes/logo-sm.php";?>
 
-    <section id="applicationHeading" class="text-center text-md-left">
+    <section id="ecisUsersHeading" class="text-center text-md-left">
       <div class="container">
         <div class="row">
           <div class="col">
@@ -37,10 +37,523 @@ $metaDesc = "Label-free Real-time Automated Cell-based assays powered by the tec
             <hr class="mt-0">
           </div>
       </div>
-    </section><!-- /applicationsHeading -->
+    </section><!-- /ecisUsersHeading -->
+
+    <section id="ecisUsersContent" class="py-4">
+      <div class="container text-center text-md-left">
+
+        <p>
+          Although Applied BioPhysics is located in the United States, ECIS technology has been providing reliable data for users
+          on a global scale. Below is a list of the many institutions and countries that utilize ECIS technology in their
+          research. Does your corner of the world reap what ECIS can sow?
+        </p>
+
+        <div class="userLinksContainer d-flex justify-content-center justify-content-md-start flex-wrap">
+          <a href="#austrailia" class="userLink">Austrailia</a>
+          <a href="#austria" class="userLink">Austria</a>
+          <a href="#belgium" class="userLink">Belgium</a>
+          <a href="#brazil" class="userLink">Brazil</a>
+          <a href="#canada" class="userLink">Canada</a>
+          <a href="#china" class="userLink">China</a>
+          <a href="#denmark" class="userLink">Denmark</a>
+          <a href="#estonia" class="userLink">Estonia</a>
+          <a href="#finland" class="userLink">Finland</a>
+          <a href="#france" class="userLink">France</a>
+          <a href="#germany" class="userLink">Germany</a>
+          <a href="#hungary" class="userLink">Hungary</a>
+          <a href="#india" class="userLink">India</a>
+          <a href="#ireland" class="userLink">Ireland</a>
+          <a href="#israel" class="userLink">Israel</a>
+          <a href="#italy" class="userLink">Italy</a>
+          <a href="#japan" class="userLink">Japan</a>
+          <a href="#korea" class="userLink">Korea</a>
+          <a href="#malawi" class="userLink">Malawi</a>
+          <a href="#malaysia" class="userLink">Malaysia</a>
+          <a href="#netherlands" class="userLink">Netherlands</a>
+          <a href="#norway" class="userLink">Norway</a>
+          <a href="#qatar" class="userLink">Qatar</a>
+          <a href="#russia" class="userLink">Russia</a>
+          <a href="#spain" class="userLink">Spain</a>
+          <a href="#sweden" class="userLink">Sweden</a>
+          <a href="#switzerland" class="userLink">Switzerland</a>
+          <a href="#taiwan" class="userLink">Taiwan</a>
+          <a href="#unitedKingdom" class="userLink">United Kingdom</a>
+          <a href="#unitedStates" class="userLink">United States</a>
+        </div><!-- /userLinksContainer -->
+
+        <div class="userCountries">
+
+          <section id="austrailia" class="pt-5">
+            <h3>Austrailia</h3>
+            <hr class="mb-4">
+            <div class="row">
+              <div class="col-md-6">
+                <p>University of Sydney</p>
+              </div>
+              <div class="col-md-6">
+                <p>University of South Australia</p>
+              </div>
+            </div>
+          </section>
+
+          <section id="austria" class="pt-5">
+            <h3>Austria</h3>
+            <hr class="mb-4">
+            <div class="row">
+              <div class="col-md-6">
+                <p>Biotec Area Krems</p>
+              </div>
+              <div class="col-md-6">
+                <p>Medical University Graz</p>
+              </div>
+              <div class="col-md-6">
+                <p>Tissue Med Biosciences GmbH & Co</p>
+              </div>
+              <div class="col-md-6">
+                <p>University of Applied Sciences Krems</p>
+              </div>
+            </div>
+          </section>
+
+          <section id="belgium" class="pt-5">
+            <h3>Belgium</h3>
+            <hr class="mb-4">
+            <div class="row">
+              <div class="col-md-6">
+                <p>Ghent University</p>
+              </div>
+              <div class="col-md-6">
+                <p>Katholieke Universiteit Leuven</p>
+              </div>
+            </div>
+          </section>
+
+          <section id="brazil" class="pt-5">
+            <h3>Brazil</h3>
+            <hr class="mb-4">
+            <div class="row">
+              <div class="col-md-6">
+                <p>Natura Invacao e Technological</p>
+              </div>
+            </div>
+          </section>
+
+          <section id="canada" class="pt-5">
+            <h3>Canada</h3>
+            <hr class="mb-4">
+            <div class="row">
+              <div class="col-md-6">
+                <p>BioPhage Inc</p>
+              </div>
+              <div class="col-md-6">
+                <p>Conseil National de Recherches</p>
+              </div>
+              <div class="col-md-6">
+                <p>Fisheries and Oceans Canada</p>
+              </div>
+              <div class="col-md-6">
+                <p>Millenium Biologix</p>
+              </div>
+              <div class="col-md-6">
+                <p>Ottawa Hospital</p>
+              </div>
+              <div class="col-md-6">
+                <p>St. Michael's Hospital</p>
+              </div>
+              <div class="col-md-6">
+                <p>Université Laval</p>
+              </div>
+              <div class="col-md-6">
+                <p>University of Alberta</p>
+              </div>
+              <div class="col-md-6">
+                <p>University of Montreal</p>
+              </div>
+              <div class="col-md-6">
+                <p>Vertex Pharmaceuticals Incorporated</p>
+              </div>
+            </div>
+          </section>
+
+          <section id="china" class="pt-5">
+            <h3>China</h3>
+            <hr class="mb-4">
+            <div class="row">
+              <div class="col-md-6">
+                <p>Capital University of Medical Sciences</p>
+              </div>
+              <div class="col-md-6">
+                <p>Central Southern University</p>
+              </div>
+              <div class="col-md-6">
+                <p>Fu Jen Catholic University</p>
+              </div>
+              <div class="col-md-6">
+                <p>Fudan University Medical School</p>
+              </div>
+              <div class="col-md-6">
+                <p>Guangdong Entry-Exit Inspection and Quarantine Bureau</p>
+              </div>
+              <div class="col-md-6">
+                <p>Guangzhou Medical University (GMU)</p>
+              </div>
+              <div class="col-md-6">
+                <p>Inner Mongolia Medical College</p>
+              </div>
+              <div class="col-md-6">
+                <p>Institute of Materia Medica</p>
+              </div>
+              <div class="col-md-6">
+                <p>Jinan University</p>
+              </div>
+              <div class="col-md-6">
+                <p>Shandong Provincial Qianfoshan Hospital</p>
+              </div>
+              <div class="col-md-6">
+                <p>Shanghai Jiao Tong University</p>
+              </div>
+              <div class="col-md-6">
+                <p>Sun Yat-sen University</p>
+              </div>
+              <div class="col-md-6">
+                <p>The Third Xiangya Hospital of Central South University</p>
+              </div>
+              <div class="col-md-6">
+                <p>ZhenJiang First People's Hospital</p>
+              </div>
+            </div>
+          </section>
+
+          <section id="denmark" class="pt-5">
+            <h3>Denmark</h3>
+            <hr class="mb-4">
+            <div class="row">
+              <div class="col-md-6">
+                <p>Novo Nordisk</p>
+              </div>
+            </div>
+          </section>
+
+          <section id="estonia" class="pt-5">
+            <h3>Estonia</h3>
+            <hr class="mb-4">
+            <div class="row">
+              <div class="col-md-6">
+                <p>Tallinn University of Technology</p>
+              </div>
+            </div>
+          </section>
+
+          <section id="finland" class="pt-5">
+            <h3>Finland</h3>
+            <hr class="mb-4">
+            <div class="row">
+              <div class="col-md-6">
+                <p>Åbo Akademi University</p>
+              </div>
+              <div class="col-md-6">
+                <p>University of Oulu</p>
+              </div>
+            </div>
+          </section>
+
+          <section id="france" class="pt-5">
+            <h3>France</h3>
+            <hr class="mb-4">
+            <div class="row">
+              <div class="col-md-6">
+                <p>Centre Hospitalier Universitaire de Nice, Inserm, U576</p>
+              </div>
+              <div class="col-md-6">
+                <p>Centre de recherche Cardiovasculaire à l'HEGP, Inserm, U970</p>
+              </div>
+            </div>
+          </section>
+
+          <section id="germany" class="pt-5">
+            <h3>Germany</h3>
+            <hr class="mb-4">
+            <div class="row">
+              <div class="col-md-6">
+                <p>Albert-Ludwigs Universitat</p>
+              </div>
+              <div class="col-md-6">
+                <p>Bayer Schering Pharma AG</p>
+              </div>
+              <div class="col-md-6">
+                <p>Beiersdorf AG</p>
+              </div>
+              <div class="col-md-6">
+                <p>Charite Universitatsmedizin Berlin</p>
+              </div>
+              <div class="col-md-6">
+                <p>CLR Chemisches Lab</p>
+              </div>
+              <div class="col-md-6">
+                <p>Fraunhofer Institute for Interfacial Engineering & Biotechnology IGB</p>
+              </div>
+              <div class="col-md-6">
+                <p>Friedrich-Alexander-Universität</p>
+              </div>
+              <div class="col-md-6">
+                <p>Friedrich-Schiller-University Jena</p>
+              </div>
+              <div class="col-md-6">
+                <p>Georg-August-Universität Göttingen</p>
+              </div>
+              <div class="col-md-6">
+                <p>Johannes Gutenberg University Mainz</p>
+              </div>
+              <div class="col-md-6">
+                <p>Johann Wolfgang Goethe-University</p>
+              </div>
+              <div class="col-md-6">
+                <p>Ludwig-Maximilians-Universität München</p>
+              </div>
+              <div class="col-md-6">
+                <p>Max-Delbrück-Centrum für Molekulare Medizin (MDC)</p>
+              </div>
+              <div class="col-md-6">
+                <p>MetaGen Pharmaceuticals GmbH</p>
+              </div>
+              <div class="col-md-6">
+                <p>Philipps-Universitat Marburg</p>
+              </div>
+              <div class="col-md-6">
+                <p>Saarland University</p>
+              </div>
+              <div class="col-md-6">
+                <p>Technical University Munchen</p>
+              </div>
+              <div class="col-md-6">
+                <p>Universitat Regensburg</p>
+              </div>
+              <div class="col-md-6">
+                <p>Universitat Wurzburg</p>
+              </div>
+              <div class="col-md-6">
+                <p>University Medical Centre Mannheim</p>
+              </div>
+              <div class="col-md-6">
+                <p>University of Tuebingen</p>
+              </div>
+              <div class="col-md-6">
+                <p>University of Witten/Herdecke</p>
+              </div>
+              <div class="col-md-6">
+                <p>WWU Munster</p>
+              </div>
+            </div>
+          </section>
+
+          <section id="hungary" class="pt-5">
+            <h3>Hungary</h3>
+            <hr class="mb-4">
+            <div class="row">
+              <div class="col-md-6">
+                <p>Semmelweis University</p>
+              </div>
+              <div class="col-md-6">
+                <p>University of Debrecen Medical School</p>
+              </div>
+            </div>
+          </section>
+
+          <section id="india" class="pt-5">
+            <h3>India</h3>
+            <hr class="mb-4">
+            <div class="row">
+              <div class="col-md-6">
+                <p>Bengal Engineering and Science University, Shibpur</p>
+              </div>
+              <div class="col-md-6">
+                <p>Central Leather Research Institute</p>
+              </div>
+              <div class="col-md-6">
+                <p>Indian Institute of Science Bangalore</p>
+              </div>
+              <div class="col-md-6">
+                <p>Indian Institute of Technology Madras</p>
+              </div>
+              <div class="col-md-6">
+                <p>Jawaharlal Nehru Centre for Advanced Scientific Research (JNCASR)</p>
+              </div>
+              <div class="col-md-6">
+                <p>National Centre for Cell Science</p>
+              </div>
+              <div class="col-md-6">
+                <p>National Institute of Immunology</p>
+              </div>
+              <div class="col-md-6">
+                <p>Osmania University</p>
+              </div>
+            </div>
+          </section>
+
+          <section id="ireland" class="pt-5">
+            <h3>Ireland</h3>
+            <hr class="mb-4">
+            <div class="row">
+              <div class="col-md-6">
+                <p>Trinity College Dublin</p>
+              </div>
+              <div class="col-md-6">
+                <p>University College Cork</p>
+              </div>
+            </div>
+          </section>
+
+          <section id="israel" class="pt-5">
+            <h3>Israel</h3>
+            <hr class="mb-4">
+            <div class="row">
+              <div class="col-md-6">
+                <p>Ben Gurion University of the Negev</p>
+              </div>
+              <div class="col-md-6">
+                <p>Haifa University</p>
+              </div>
+              <div class="col-md-6">
+                <p>Rambam Hospital</p>
+              </div>
+              <div class="col-md-6">
+                <p>Teva Pharmaceutical Industries Ltd</p>
+              </div>
+            </div>
+          </section>
+
+          <section id="italy" class="pt-5">
+            <h3>Italy</h3>
+            <hr class="mb-4">
+            <div class="row">
+              <div class="col-md-6">
+                <p>Analitica DeMori Srl.</p>
+              </div>
+              <div class="col-md-6">
+                <p>European Commission Joint Research Centre</p>
+              </div>
+            </div>
+          </section>
+
+          <section id="japan" class="pt-5">
+            <h3>Japan</h3>
+            <hr class="mb-4">
+            <div class="row">
+              <div class="col-md-6">
+                <p>Astellas Pharma Inc.</p>
+              </div>
+              <div class="col-md-6">
+                <p>Fukui Medical School of Japan</p>
+              </div>
+              <div class="col-md-6">
+                <p>Kawasaki Medical School</p>
+              </div>
+              <div class="col-md-6">
+                <p>National Cancer Center</p>
+              </div>
+              <div class="col-md-6">
+                <p>National Center for Child Health and Development</p>
+              </div>
+              <div class="col-md-6">
+                <p>National Center for Global Health and Medicine Research Institute</p>
+              </div>
+              <div class="col-md-6">
+                <p>National Cerebral and Cardiovascular Center</p>
+              </div>
+              <div class="col-md-6">
+                <p>Okayama University</p>
+              </div>
+              <div class="col-md-6">
+                <p>Osaka City University Medical School</p>
+              </div>
+              <div class="col-md-6">
+                <p>Otsuka Pharmaceutical Co.</p>
+              </div>
+              <div class="col-md-6">
+                <p>University of Tokyo</p>
+              </div>
+              <div class="col-md-6">
+                <p>University of Tsukuba</p>
+              </div>
+            </div>
+          </section>
+
+          <section id="korea" class="pt-5">
+            <h3>Korea</h3>
+            <hr class="mb-4">
+            <div class="row">
+              <div class="col-md-6">
+                <p>Chungbuk National University</p>
+              </div>
+              <div class="col-md-6">
+                <p>DaeJeon Science High School</p>
+              </div>
+              <div class="col-md-6">
+                <p>Gachon University</p>
+              </div>
+              <div class="col-md-6">
+                <p>Jeju National University</p>
+              </div>
+              <div class="col-md-6">
+                <p>Kyungwon University</p>
+              </div>
+              <div class="col-md-6">
+                <p>Yonsei University College of Medicine</p>
+              </div>
+            </div>
+          </section>
+
+          <section id="malawi" class="pt-5">
+            <h3>Malawi</h3>
+            <hr class="mb-4">
+            <div class="row">
+              <div class="col-md-6">
+                <p>Queen Elizabeth Central Hospital</p>
+              </div>
+            </div>
+          </section>
+
+          <section id="malaysia" class="pt-5">
+            <h3>Malaysia</h3>
+            <hr class="mb-4">
+            <div class="row">
+              <div class="col-md-6">
+                <p>University of Malaya</p>
+              </div>
+            </div>
+          </section>
+
+          <section id="netherlands" class="pt-5">
+            <h3>Netherlands</h3>
+            <hr class="mb-4">
+            <div class="row">
+              <div class="col-md-6">
+                <p>AMC University of Amsterdam</p>
+              </div>
+              <div class="col-md-6">
+                <p>Leiden University Medical Center</p>
+              </div>
+              <div class="col-md-6">
+                <p>Sanquin Research CLB</p>
+              </div>
+              <div class="col-md-6">
+                <p>UMC Utrecht</p>
+              </div>
+              <div class="col-md-6">
+                <p>University Medical Center Groningen</p>
+              </div>
+              <div class="col-md-6">
+                <p>VU University Medical Center</p>
+              </div>
+            </div>
+          </section>
+
+        </div><!-- /userCountries -->
+    
+      </div>
+    </section>
 
     
-
     <?php include_once "includes/footer.php";?>
 
     <!-- Scroll to top -->
