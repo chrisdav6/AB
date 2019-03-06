52636fe32c25db2be8e325a15e3ddbf357b7d7d0
Chris Davis <chris.davis5440@gmail.com>
2019-03-06 10:09:55 -0500

    completed techSupport page

--- techSupport.php
+++ techSupport.php
@@ -55,9 +55,49 @@ $metaDesc = "Label-free Real-time Automated Cell-based assays powered by the tec
           </div>
         </div>
 
-        <h4 class="mt-5 mb-4 secondary-title">Manuals</h4>
+        <h4 class="mt-5 mt-lg-0 mb-4 secondary-title">Manuals</h4>
 
-        <p><a href="public/pdf/ECIS_Operation_Manual_v1_2_123.pdf" class="link text-dark" target="_blank">ECIS Operation Manual</a></p>
+        <div class="supportDocs">
+          <p><a href="public/pdf/ECIS_Operation_Manual_v1_2_123.pdf" class="link text-dark" target="_blank"><i class="far fa-file-pdf mr-2"></i> ECIS Operation Manual</a></p>
+          <p><a href="public/pdf/ECISHandbook.pdf" class="link text-dark" target="_blank"><i class="far fa-file-pdf mr-2"></i> ECIS Handbook</a></p>
+          <p><a href="public/pdf/P-FlowPumpManualRevD.pdf" class="link text-dark" target="_blank"><i class="far fa-file-pdf mr-2"></i> P-Flow Pump Manual</a></p>
+          <p><a href="public/pdf/PerfusionIN-1090X_pump_system manual.pdf" class="link text-dark" target="_blank"><i class="far fa-file-pdf mr-2"></i> Perfusion IN-1090X Pump System Manual</a></p>
+          <p><a href="public/pdf/IncubatorPortSheldonInstructions.pdf" class="link text-dark" target="_blank"><i class="far fa-file-pdf mr-2"></i> Incubator Port Sheldon Instructions</a></p>
+        </div>
+
+        <h4 class="mt-5 mb-4 secondary-title">Journal Articles</h4>
+
+        <div class="supportDocs">
+          <p><a href="https://link.springer.com/article/10.1007/s00424-014-1674-0?view=classic" class="link text-dark" target="_blank">Impedance analysis of GPCR-mediated changes in endothelial barrier function: Overview, and fundamental considerations for stable and reproducible measurements</a></p>
+        </div>
+
+        <h4 class="mt-5 mb-4 secondary-title">Books</h4>
+
+        <div class="supportDocs">
+          <p><a href="https://www.springer.com/us/book/9789400749269" class="link text-dark" target="_blank">Electric Cell-Substrate Impedance Sensing and Cancer Metastasis" is available from Springer Netherlands</a></p>
+        </div>
+
+        <h4 class="mt-5 mb-4 secondary-title">JOVE Videos</h4>
+
+        <div class="supportDocs">
+          <p><a href="https://www.jove.com/video/51300/electric-cell-substrate-impedance-sensing-for-quantification" class="link text-dark" target="_blank"><i class="fas fa-video mr-2"></i> Electric Cell-substrate Impedance Sensing for the Quantification of Endothelial Proliferation, Barrier Function, and Motility (12min)</a></p>
+          <p><a href="https://www.jove.com/video/3840/measurement-of-cellular-chemotaxis-with-ecistaxis" class="link text-dark" target="_blank"><i class="fas fa-video mr-2"></i> Measurement of Cellular Chemotaxis with ECIS/Taxis (11min)</a></p>
+        </div>
+
+        <h4 class="mt-5 mb-4 secondary-title">Tutorial Notes</h4>
+
+        <div class="supportDocs">
+          <p><a href="public/pdf/ImprovingExperimentRepeatabilitywithCysteine.pdf" class="link text-dark" target="_blank"><i class="far fa-file-pdf mr-2"></i> Improving Experiment Repeatability with Cysteine</a></p>
+          <p><a href="public/pdf/InoculationOfArrays.pdf" class="link text-dark" target="_blank"><i class="far fa-file-pdf mr-2"></i> Inoculation of Arrays</a></p>
+          <p><a href="public/pdf/Pre-CoatingElectrodeswithDefinedProteinCoats.pdf" class="link text-dark" target="_blank"><i class="far fa-file-pdf mr-2"></i> Pre-Coating Electrodes with Defined Protein Coats</a></p>
+          <p><a href="public/pdf/AnAlternativeWaytoObtainGoodCellDistributiononECISArrays.pdf" class="link text-dark" target="_blank"><i class="far fa-file-pdf mr-2"></i> An Alternative Way to Obtain Good Cell Distribution on ECIS Arrays</a></p>
+          <p><a href="public/pdf/AddingCompoundsToStudyTheirEffectonECISMeasurements.pdf" class="link text-dark" target="_blank"><i class="far fa-file-pdf mr-2"></i> Adding Compounds to Study their Effect on ECIS Measurements</a></p>
+          <p><a href="public/pdf/ECIS_Microscopy_V2_DINA4.pdf" class="link text-dark" target="_blank"><i class="far fa-file-pdf mr-2"></i> Phase-Contrast & Fluorescence Microscopy Tutorial</a></p>
+          <p><a href="public/pdf/ChoiceOfProgramForECISTimeCourseDataAcquisition.pdf" class="link text-dark" target="_blank"><i class="far fa-file-pdf mr-2"></i> Choice of Program for ECIS Time Course Data Acquisition</a></p>
+          <p><a href="public/pdf/ChoiceOfProgramForECISTimeCourseDataAcquisition.pdf" class="link text-dark" target="_blank"><i class="far fa-file-pdf mr-2"></i> Don't Forget Capacitance</a></p>
+          <p><a href="public/pdf/DontOverlookMicromotion.pdf" class="link text-dark" target="_blank"><i class="far fa-file-pdf mr-2"></i> Don't Overlook Micromotion</a></p>
+          <p><a href="public/pdf/AN12_Avoiding_evaporation.pdf" class="link text-dark" target="_blank"><i class="far fa-file-pdf mr-2"></i> Avoiding Evaporation (ibidi)</a></p>
+        </div>
         
       </div>
     </section><!-- /techSupportContent -->
