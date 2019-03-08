b72ae435e8353920c663988e38a03cbc97b51e5f
Chris Davis <chris.davis5440@gmail.com>
2019-03-08 08:46:32 -0500

    added options pages

---
+++ CO2Incubator.php
@@ -0,0 +1,64 @@
+<?php
+$title = "Applied Biophysics";
+$metaDesc = "Label-free Real-time Automated Cell-based assays powered by the technology of ECIS. Developed by Nobel laureate Ivar
+    Giaever and Charles Keese ECIS uses impedance sensing to monitor the behavior of cells as they grow in culture.";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="<?php echo $metaDesc; ?>">
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,600,700,900" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="public/css/bootstrap.css" type="text/css">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="public/css/style.css">
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="public/img/favicon.ico">
+    <title><?php echo $title; ?></title>
+  </head>
+
+  <body class="d-flex flex-column">
+
+    <?php include_once "includes/navbar.php";?>
+    <?php include_once "includes/logo-sm.php";?>
+
+    <section id="CO2IncubatorHeading" class="mb-2 text-center text-md-left">
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <h1 class="display-3">CO<sub>2</sub> Incubator</h1>
+            <hr class="my-2">
+            <h2>CO<sub>2</sub> Compact Tissue Culture Incubator</h2>
+          </div>
+        </div>
+      </div>
+    </section><!-- /CO2IncubatorHeading -->
+
+    <section id="CO2IncubatorContent" class="py-4 mb-5">
+      <div class="container text-center text-md-left">
+
+        
+        
+      </div>
+    </section><!-- /CO2IncubatorContent -->
+
+    <?php include_once "includes/footer.php";?>
+
+    <!-- Scroll to top -->
+    <button class="btn btn-outline-dark" id="scrollBtn"><i class="fas fa-angle-up fa-2x"></i></button>
+
+    <script src="public/js/jquery.min.js"></script>
+    <script src="public/js/bootstrap.bundle.min.js"></script>
+    <script src="public/js/util.js"></script>
+    <script src="public/js/smooth-scroll.min.js"></script>
+    <script src="public/js/scripts.js"></script>
+  </body>
+
+</html>
