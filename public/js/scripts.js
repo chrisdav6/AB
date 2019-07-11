e83c936fef7fb394d408e830159211d00c178ae8
Brian Detweiler <support@briandetweiler.com>
2019-07-11 11:31:18 -0400

    fixed a console error from trying to reference underscores while not on publications page

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -87,88 +87,91 @@ $(function() {
 jQuery(document).ready(function() {
   (function($) {
     var $publicationsTable = $('#publications-table');
-    var $publicationLabels = $('#publication-labels > a:not(".submitPubBtn")');
-    var publicationsDetailsTemplate = _.template(
-        $('#publication-details-template').html());
-    var $publicationsModal = $('#publications-modal');
 
-    var publicationsDataTable = $publicationsTable.
-        DataTable({
-          order: [[0, 'desc']],
-          columnDefs: [
-            {
-              targets: [1, 2],
-              visible: false,
-              searchable: true,
+    if ($publicationsTable.length) {
+      var $publicationLabels = $('#publication-labels > a:not(".submitPubBtn")');
+      var publicationsDetailsTemplate = _.template(
+          $('#publication-details-template').html());
+      var $publicationsModal = $('#publications-modal');
+
+      var publicationsDataTable = $publicationsTable.
+          DataTable({
+            order: [[0, 'desc']],
+            columnDefs: [
+              {
+                targets: [1, 2],
+                visible: false,
+                searchable: true,
+              },
+              {
+                targets: 0,
+                orderData: [1],
+              },
+            ],
+            ordering: false,
+            dom: '<"row"<"col-sm-12"f>>' +
+                '<"row"<"col-sm-5"i><"col-sm-7"p>>' +
+                '<"row"<"col-sm-12"tr>>' +
+                '<"row"<"col-sm-5"i><"col-sm-7"p>>',
+            initComplete: function() {
+              $publicationsTable.removeAttr('hidden').css('width', '100%');
             },
-            {
-              targets: 0,
-              orderData: [1],
-            },
-          ],
-          ordering: false,
-          dom: '<"row"<"col-sm-12"f>>' +
-              '<"row"<"col-sm-5"i><"col-sm-7"p>>' +
-              '<"row"<"col-sm-12"tr>>' +
-              '<"row"<"col-sm-5"i><"col-sm-7"p>>',
-          initComplete: function() {
-            $publicationsTable.removeAttr('hidden').css('width', '100%');
-          },
-        });
+          });
 
-    $publicationLabels.on('click', function(e) {
-      e.preventDefault();
-      var $this = $(this);
-      var filter = $this.data('filter');
-      var $labelsFilterMessage = $('#labels-filter-message');
+      $publicationLabels.on('click', function(e) {
+        e.preventDefault();
+        var $this = $(this);
+        var filter = $this.data('filter');
+        var $labelsFilterMessage = $('#labels-filter-message');
 
-      if ($this.hasClass('active')) {
-        publicationsDataTable.columns().search('').draw();
-        $this.removeClass('active');
-        $labelsFilterMessage.html(null);
-      } else {
-        publicationsDataTable.columns(2).
-            search(filter, false, false, false).
-            draw();
-        $publicationLabels.filter('.active').removeClass('active');
-        $this.addClass('active');
-        $labelsFilterMessage.html('In ' + filter);
-      }
-    });
+        if ($this.hasClass('active')) {
+          publicationsDataTable.columns().search('').draw();
+          $this.removeClass('active');
+          $labelsFilterMessage.html(null);
+        } else {
+          publicationsDataTable.columns(2).
+              search(filter, false, false, false).
+              draw();
+          $publicationLabels.filter('.active').removeClass('active');
+          $this.addClass('active');
+          $labelsFilterMessage.html('In ' + filter);
+        }
+      });
 
-    $publicationsModal.on('show.bs.modal', function(e) {
-      var $modal = $(this);
+      $publicationsModal.on('show.bs.modal', function(e) {
+        var $modal = $(this);
 
-      var $publication = $(e.relatedTarget).closest('.publication');
-      var authors = $publication.find('.publication-authors').html();
-      var title = $publication.find('.publication-title').html();
-      var publishedIn = $publication.find('.publication-published-in').html();
-      var volume = $publication.find('.publication-volume').html();
-      var issue = $publication.find('.publication-issue').html();
-      var date = $publication.find('.publication-year').html();
-      var pages = $publication.find('.publication-pages').html();
-      var doi = $publication.find('.publication-doi')[0];
-      var abstract = $publication.data('abstract');
-      var labels = $publication.find('.publication-labels').html();
+        var $publication = $(e.relatedTarget).closest('.publication');
+        var authors = $publication.find('.publication-authors').html();
+        var title = $publication.find('.publication-title').html();
+        var publishedIn = $publication.find('.publication-published-in').html();
+        var volume = $publication.find('.publication-volume').html();
+        var issue = $publication.find('.publication-issue').html();
+        var date = $publication.find('.publication-year').html();
+        var pages = $publication.find('.publication-pages').html();
+        var doi = $publication.find('.publication-doi')[0];
+        var abstract = $publication.data('abstract');
+        var labels = $publication.find('.publication-labels').html();
 
-      $modal.find('.modal-body').html(publicationsDetailsTemplate({
-        authors: authors,
-        title: title,
-        publishedIn: publishedIn,
-        volume: volume,
-        issue: issue,
-        date: date,
-        pages: pages,
-        doi: doi ? doi.outerHTML : null,
-        abstract: abstract,
-        labels: labels,
-      }));
-    });
+        $modal.find('.modal-body').html(publicationsDetailsTemplate({
+          authors: authors,
+          title: title,
+          publishedIn: publishedIn,
+          volume: volume,
+          issue: issue,
+          date: date,
+          pages: pages,
+          doi: doi ? doi.outerHTML : null,
+          abstract: abstract,
+          labels: labels,
+        }));
+      });
 
-    $publicationsModal.on('hidden.bs.modal', function(e) {
-      var $modal = $(this);
-      $modal.find('.modal-body').html(null);
-    });
+      $publicationsModal.on('hidden.bs.modal', function(e) {
+        var $modal = $(this);
+        $modal.find('.modal-body').html(null);
+      });
+    }
 
   })(jQuery);
 });
