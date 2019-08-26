6ecb3114eb7b94cc6505757b113889645155debc
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2019-08-26 08:28:19 -0400

    Updated hompage meta description

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
   $title = "Applied Biophysics";
   $metaDesc = "Label-free Real-time Automated Cell-based assays powered by the technology of ECIS. Developed by Nobel laureate Ivar
-    Giaever and Charles Keese ECIS uses impedance sensing to monitor the behavior of cells as they grow in culture.";
+    Giaever and Charles Keese.";
 ?>
 
 <!DOCTYPE html>
