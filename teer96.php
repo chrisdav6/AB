4cc156ce224406600f0d18ac07b725a81be01536
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2020-01-21 14:49:01 -0500

    added TEER96 data sheet and manual

--- teer96.php
+++ teer96.php
@@ -217,15 +217,15 @@
           <hr class="mt-1">
           <div class="row mt-4">
             <div class="col-md-3 pb-4 pb-md-0">
-              <a href="public/pdf/TEER24DataSheetR4.pdf" target="_blank" data-toggle="tooltip" data-placement="right"
+              <a href="public/pdf/TEER96DataSheetR2.pdf" target="_blank" data-toggle="tooltip" data-placement="right"
                 title="Download TEER96 Data Sheet">
-                <img class="img-fluid" src="public/img/teer24DataSheet.jpg" alt="Download TEER96 Data Sheet">
+                <img class="img-fluid" src="public/img/teer96DataSheet.jpg" alt="Download TEER96 Data Sheet">
               </a>
             </div>
             <div class="col-md-3">
-              <a href="public/pdf/TEER24ManualR6.pdf" target="_blank" data-toggle="tooltip" data-placement="right"
+              <a href="public/pdf/TEER96ManualR2.pdf" target="_blank" data-toggle="tooltip" data-placement="right"
                 title="Download TEER96 Manual">
-                <img class="img-fluid" src="public/img/teer24Manual.jpg" alt="Download TEER96 Manual">
+                <img class="img-fluid" src="public/img/teer96Manual.jpg" alt="Download TEER96 Manual">
               </a>
             </div>
           </div>
