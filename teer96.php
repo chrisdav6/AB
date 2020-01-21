3023bbb30c99f17e0e806b706269d99f0a33792d
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2020-01-21 08:58:53 -0500

    Started building TEER96 page

---
+++ teer96.php
@@ -0,0 +1,333 @@
+<?php
+  $title = "TEER96 - Barrier Function Measurements - Applied BioPhysics";
+  $metaDesc = "The ECIS TEER96 is designed to carry out traditional trans-epithelial/endothelial electrical resistance
+  measurements using ECIS ";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name="description" content="<?php echo $metaDesc; ?>">
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Lato:300,400,600,700,900" rel="stylesheet">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
+  <!-- Bootstrap -->
+  <link rel="stylesheet" href="public/css/bootstrap.css" type="text/css">
+  <!-- Custom CSS -->
+  <link rel="stylesheet" href="public/css/style.css">
+  <!-- Favicon -->
+  <link rel="icon" type="image/x-icon" href="public/img/favicon.ico">
+  <title><?php echo $title; ?></title>
+</head>
+
+<body class="d-flex flex-column">
+
+  <?php include_once "includes/navbar.php";?>
+  <?php include_once "includes/logo-sm.php";?>
+
+  <section id="productHeading" class="pb-4 text-center text-md-left">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <h1 class="display-2">TEER96</h1>
+          <hr class="my-2">
+          <h2>96 Well TEER Measurement System</h2>
+        </div>
+      </div>
+    </div>
+  </section><!-- /productHeading -->
+
+  <section id="productCarousel" class="py-4">
+    <div class="container">
+      <div class="row">
+
+        <div class="col-lg-5">
+
+          <div id="carouselExampleIndicators" class="carousel slide border" data-ride="carousel">
+            <div class="carousel-inner">
+              <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+              </ol>
+
+              <div class="carousel-item active">
+                <img class="d-block w-100" src="public/img/teer96Front.jpg"
+                  alt="TEER96 Barrier Function Measurement System">
+              </div>
+
+              <div class="carousel-item">
+                <img class="d-block w-100" src="public/img/teer96Station.jpg"
+                  alt="TEER96 Barrier Function Measurement System">
+              </div>
+
+              <div class="carousel-item">
+                <img class="d-block w-100" src="public/img/teer24Data.jpg"
+                  alt="TEER96 Barrier Function Measurement System Data">
+              </div>
+
+            </div>
+            <a class="carousel-control-prev" data-scroll-ignore data-toggle="tab" href="#carouselExampleIndicators"
+              role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" data-scroll-ignore data-toggle="tab" href="#carouselExampleIndicators"
+              role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+          </div>
+
+        </div>
+
+        <div class="col-lg-7 text-center text-md-left mt-4">
+          <h2>Description</h2>
+          <hr class="mt-1">
+          <p>
+            How can TEER assays for barrier function be made easier and more accurate? By making them automated and
+            reporting measurements in real time. The all new TEER96 by Applied Biophysics, Inc. A more effective
+            approach to a traditionally complicated assay.
+          </p>
+          <a href="#dataSheets" class="btn btn-outline-success productBtn mr-0 mr-md-1" role="button">Download Data
+            Sheet</a>
+          <a href="#" class="btn btn-success productBtn" role="button" data-id="Order Info Form" data-toggle="modal"
+            data-target="#orderInfo">Order Info</a>
+        </div>
+
+      </div>
+    </div>
+  </section> <!-- /productCarousel -->
+
+  <section id="productInfo" class="py-5 mb-5 text-center text-md-left">
+    <div class="container">
+
+      <div class="row">
+        <div class="col">
+          <h2>Overview</h2>
+          <hr class="mt-1">
+          <p>
+            The ECIS TEER96 is designed to carry out traditional trans-epithelial/endothelial electrical resistance
+            measurements using ECIS (electric cell-substrate impedance sensing) technology to monitor the barrier
+            function of cell layers grown on membrane insert filters. Data is collected continuously from up to 96
+            independent wells and reported as real-time barrier function changes in ohm-cm<sup>2</sup>.
+          </p>
+          <h5 class="mt-4">Benefits</h5>
+          <ul class="list-unstyled mt-3 ml-0 ml-md-3">
+            <li>- Automated TEER measurements from under 10 to 1,000 ohm-cm<sup>2</sup> in 96 wells</li>
+            <li>- Can run in single point measurement or continuous mode</li>
+            <li>- Uses Millipore<sup>&reg;</sup> and Corning<sup>&reg;</sup> 96-well filter plates</li>
+            <li>- Data recorded and visualized in real time under incubator conditions</li>
+            <li>- Accurate measuring of endothelial barrier function</li>
+            <li>- Long term data collection capabilities</li>
+            <li>- Easily sterilizable, disposable array and autoclavable stainless dipping pins</li>
+            <li>- Fast barrier function dynamics monitoring</li>
+            <li>- Non-invasive &amp; label free</li>
+            <li>- Highly reproducible</li>
+            <li>- User friendly software</li>
+            <li>- Data output in CSV or graphical (jpg, tif)</li>
+            <li>- Create figures and plots of data</li>
+          </ul>
+          <h5 class="mt-4">System Includes:</h5>
+          <ul class="list-unstyled mt-3 ml-0 ml-md-3">
+            <li>- 96 well channel station located in or outside the CO<sub>2</sub> incubator</li>
+            <li>- External control module</li>
+            <li>- Laptop PC</li>
+            <li>- ECIS control, acquisition, and display software</li>
+            <li>- Validation test array</li>
+            <li>- Two 96 well gold coated dipping pin assemblies</li>
+            <li>- Six 96-well 0.4µm Millipore filter plates</li>
+          </ul>
+          <p class="mt-4">
+            Non-invasive measurements may be made continuously for days and even weeks with TEER96. The fixed position
+            of electrodes throughout the measurements eliminates operator variability allowing precise and repeatable
+            measurements of cell layers with weak barrier function (&lt;10 ohm-cm<sup>2</sup>).
+          </p>
+          <p class="mt-4">
+            The TEER96 accepts standard Millipore<sup>&reg;</sup> and Corning<sup>&reg;</sup> 96-well filter plates.
+            These are placed on the platen of the TEER96. An optical switch senses the array is in place and
+            automatically moves the array into the station and precisely moves the plate up to the dipping pins. Each
+            well has two gold coated dipping pins, one for the counter electrode and one over the filter to take the
+            measurement.
+          </p>
+          <p>
+            The system can take just one reading of each well per plate or take continuous data on a plate over days or
+            weeks. The system can be programmed to include a wash cycle after a certain number of arrays are run. The
+            system includes a bar code reader, the bar code data is integrated with the data file.
+          </p>
+        </div>
+      </div>
+
+      <div class="row mt-5">
+        <div class="col">
+
+          <ul class="nav nav-tabs" id="myTab" role="tablist">
+            <li class="nav-item">
+              <a class="nav-link active text-dark" id="specs-tab" data-scroll-ignore data-toggle="tab" href="#specs"
+                role="tab" aria-controls="specs" aria-selected="false">Specifications</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-dark" id="video-tab" data-scroll-ignore data-toggle="tab" href="#video" role="tab"
+                aria-controls="video" aria-selected="false">Video</a>
+            </li>
+            <!-- <li class="nav-item">
+              <a class="nav-link text-dark" id="options-tab" data-scroll-ignore data-toggle="tab" href="#options"
+                role="tab" aria-controls="options" aria-selected="false">Options</a>
+            </li> -->
+          </ul>
+
+          <div class="tab-content" id="myTabContent">
+            <div class="tab-pane fade show active" id="specs" role="tabpanel" aria-labelledby="specs-tab">
+              <p class="mt-4">
+                <ul class="list-unstyled mt-3 ml-0 ml-md-3">
+                  <li>- Gold pin electrodes</li>
+                  <li>- 75 Hz sinusoidal signal</li>
+                  <li>- Power: 3 watts; 12 V dc</li>
+                  <li>- Station: X x X x X cm, X kg</li>
+                  <li>- Controller: 21.5 x 18 x 4.5 cm, 2 kg</li>
+                  <li>- motion control box X x X x X</li>
+                  <li>- Connects to laptop via USB</li>
+                  <li>- Windows 10 OS</li>
+                </ul>
+              </p>
+            </div>
+            <div class="tab-pane fade" id="video" role="tabpanel" aria-labelledby="video-tab">
+              <p class="mt-4">
+                <div class="youtubeEmbed" id="aIT_ldWSNvY"></div>
+              </p>
+              <p class="mt-4">
+                <div class="youtubeEmbed" id="-p-1MbOJ29U"></div>
+              </p>
+            </div>
+            <!-- <div class="tab-pane fade" id="options" role="tabpanel" aria-labelledby="options-tab">
+              <p class="mt-4">Descriptions and links of optional add-on products and/or training</p>
+            </div> -->
+          </div>
+        </div>
+      </div>
+
+      <div id="dataSheets" class="row mt-5">
+        <div class="col">
+          <h2>Data Sheets <i class="far fa-file-pdf ml-1"></i></h2>
+          <hr class="mt-1">
+          <div class="row mt-4">
+            <div class="col-md-3 pb-4 pb-md-0">
+              <a href="public/pdf/TEER24DataSheetR4.pdf" target="_blank" data-toggle="tooltip" data-placement="right"
+                title="Download TEER96 Data Sheet">
+                <img class="img-fluid" src="public/img/teer24DataSheet.jpg" alt="Download TEER96 Data Sheet">
+              </a>
+            </div>
+            <div class="col-md-3">
+              <a href="public/pdf/TEER24ManualR6.pdf" target="_blank" data-toggle="tooltip" data-placement="right"
+                title="Download TEER96 Manual">
+                <img class="img-fluid" src="public/img/teer24Manual.jpg" alt="Download TEER96 Manual">
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </section>
+
+  <?php include_once "includes/footer.php";?>
+
+  <!-- Scroll to top -->
+  <button class="btn btn-outline-dark" id="scrollBtn"><i class="fas fa-angle-up fa-2x"></i></button>
+
+  <!-- Order Info Modal -->
+  <div class="modal fade" id="orderInfo" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h4 class="modal-title">Order Information</h4>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <form id="orderInfoForm" action="/orderInfoProcess.php" method="post">
+            <div class="form-group">
+              <label for="name">Name</label>
+              <input type="text" class="form-control form-control-lg" name="name" id="name" required>
+            </div>
+            <div class="form-group">
+              <label for="company">Institution</label>
+              <input type="text" class="form-control form-control-lg" name="institution" id="institution" required>
+            </div>
+            <div class="form-group">
+              <label for="text">Phone Number</label>
+              <input type="text" class="form-control form-control-lg" name="phone" id="phone" required>
+            </div>
+            <div class="form-group">
+              <label for="email">Email Address</label>
+              <input type="email" class="form-control form-control-lg" name="email" id="email" required>
+            </div>
+            <div class="form-group">
+              <label for="system">System Interested In</label>
+              <input type="text" class="form-control form-control-lg" name="system" id="system" required>
+            </div>
+            <div class="form-group d-none">
+              <!-- Honey -->
+              <input type="text" class="form-control form-control-lg" name="fax" id="fax">
+            </div>
+            <div class="form-group mt-4">
+              <label class="d-block">Would you like to Demo or Purchase?</label>
+              <div class="form-check form-check-inline">
+                <input class="form-check-input demoOrPurchase" type="radio" name="demoOrPurchase"
+                  id="demoOrPurchaseDemo" value="Demo" checked>
+                <label class="form-check-label" for="demoOrPurchaseDemo">Demo</label>
+              </div>
+              <div class="form-check form-check-inline">
+                <input class="form-check-input demoOrPurchase" type="radio" name="demoOrPurchase"
+                  id="demoOrPurchasePurchase" value="Purchase">
+                <label class="form-check-label" for="demoOrPurchasePurchase">Purchase</label>
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="comments">Comments</label>
+              <textarea class="form-control form-control-lg" name="comments" id="comments"></textarea>
+            </div>
+
+            <div class="row">
+              <div class="col d-flex flex-column">
+                <button id="orderInfoBtn" class="btn ecisBtn btn-lg submit-btn btn-block mt-3 ml-auto">Submit</button>
+                <i class="fas fa-spinner fa-2x fa-spin mt-4 mx-auto"></i>
+                <!--Initially hidden in the css-->
+                <span class="submitMessage text-center mt-4 mb-3 mx-auto">Thank you, your request has been sent</span>
+                <!--Initially hidden in the css-->
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <script src="public/js/jquery.min.js"></script>
+  <script src="public/js/bootstrap.bundle.min.js"></script>
+  <script src="public/js/util.js"></script>
+  <script src="public/js/smooth-scroll.min.js"></script>
+  <script src="public/js/formsHandling.js"></script>
+  <script src="public/js/scripts.js"></script>
+
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-108411540-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'UA-108411540-1');
+  </script>
+</body>
+
+</html>
