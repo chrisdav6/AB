e7e9a553bfbf3d7f453382d7696bfd7544592d85
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2020-03-03 11:32:23 -0500

    Updated webinar

--- webinars.php
+++ webinars.php
@@ -201,7 +201,7 @@
         <a class="register" href="https://appliedbiophysics.webex.com/webappng/sites/appliedbiophysics/meeting/info/c1e94ef02d334dd280bbb08d585b1e5e?localOccurrenceTime=2020-02-18%2011%3A00%3A00" target="_blank">Register</a>
       </div>
 
-      <hr>-->
+      <hr>
 
       <div class="webinar">
         <h4 class="webinar-title">Real-time Electroporation and Monitoring – 11:00 AM EST</h4>
@@ -211,7 +211,7 @@
         <a class="register" href="https://appliedbiophysics.webex.com/webappng/sites/appliedbiophysics/meeting/info/e7062af6d102428ab45f09c931e5a58e?localOccurrenceTime=2020-03-03%2011%3A00%3A00" target="_blank">Register</a>
       </div>
 
-      <hr>
+      <hr>-->
 
       <div class="webinar">
         <h4 class="webinar-title">Cell Attachment and Spreading Measurements – 11:00 AM EDT</h4>
