f80d64f3b479da32797926b0c5204a6ae589aa34
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2021-03-05 11:41:41 -0500

    Updated tradeshow announcement

--- index.php
+++ index.php
@@ -91,9 +91,11 @@
             <div class="card text-center">
               <div class="card-body">
                 <h3 class="card-title">Tradeshows</h3>
-                <h4 class="mb-1">March, 2021</h4>
+                <h4 class="mb-1">No Scheduled Shows</h4>
+                <p class="card-text mb-0">Please check back soon.</p>
+                <!-- <h4 class="mb-1">March, 2021</h4>
                 <p class="card-text mb-0">SOT Annual Meeting and ToxExpo</p>
-                <p class="card-text mb-0">Virtual Event</p>
+                <p class="card-text mb-0">Virtual Event</p> -->
               </div>
             </div>
           </a>
