c8200a1df63b8deaeaf33f555d450cfe680477d1
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2022-04-01 10:31:04 -0400

    Added registered trademark to ZTheta title on ZTheta page

--- ztheta.php
+++ ztheta.php
@@ -34,7 +34,7 @@
     <div class="container">
       <div class="row">
         <div class="col">
-          <h1 class="display-2">ZTheta</h1>
+          <h1 class="display-2">ZTheta<span class="registered">®</span></h1>
           <hr class="my-2">
           <h2>16 or 96 Well Array Station</h2>
         </div>
