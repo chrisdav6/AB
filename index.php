7b6186c319aef7070253384535694b8a922d7f3f
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2024-01-16 11:06:26 -0500

    Removed Closure message MLK day

--- index.php
+++ index.php
@@ -47,13 +47,13 @@
     <!-- </a> -->
   </div>
 
-  <div class="holiday">
+  <!-- <div class="holiday">
     <div class="container py-3 text-center">
       <h5 class="mb-0">
         Applied Biophysics will be closed on January 15, 2024, for Martin Luther King Jr. Day
       </h5>
     </div>
-  </div>
+  </div> -->
 
   <!-- <div class="holiday">
     <div class="container py-3 text-center">
