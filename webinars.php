f0d2124c7b6dbbc6bedc682c6368e74aa0896212
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2024-11-12 11:14:24 -0500

    Updated webinar

--- webinars.php
+++ webinars.php
@@ -175,7 +175,7 @@
         <a class="register" href="https://appliedbiophysics.webex.com/weblink/register/r634c036be457668b01ec560f273f3ce0" target="_blank">Register</a>
       </div>
 
-      <hr>-->
+      <hr>
 
       <div class="webinar">
         <h4 class="webinar-title">ECIS Theory – 11:00 AM EST</h4>
@@ -184,7 +184,7 @@
         <a class="register" href="https://appliedbiophysics.webex.com/weblink/register/r01f4c30d4a99727f0d8ccfb256e4a756" target="_blank">Register</a>
       </div>
 
-      <hr>
+      <hr>-->
 
       <div class="webinar">
         <h4 class="webinar-title">Cell Invasion / Extravasation Assays – 11:00 AM EST</h4>
