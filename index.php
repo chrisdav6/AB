68b4844b4f05f3e176ed256e33ed4787a91def73
Chris Davis <chris.davis5440@gmail.com>
2026-01-06 15:24:10 -0500

    Updated webinar

--- index.php
+++ index.php
@@ -112,8 +112,8 @@
             <div class="card text-center">
               <div class="card-body">
                 <h3 class="card-title">Webinars</h3>
-                <h4 class="mb-1">January 6, 2026</h4>
-                <p class="card-text mb-0">ECIS Theory</p>
+                <h4 class="mb-1">January 20, 2026</h4>
+                <p class="card-text mb-0">Cell Invasion / Extravasation Assays</p>
                 <p class="card-text mb-0">11:00AM EST</p>
                 <!-- Extra Event -->
                 <!-- <h4 class="mb-1 mt-3">May 17, 2022</h4>
--- webinars.php
+++ webinars.php
@@ -134,14 +134,14 @@
 
       <h4 class="mt-5 mb-3 secondary-title">2026 Webinar Schedule</h4>
 
-      <div class="webinar">
+      <!-- <div class="webinar">
         <h4 class="webinar-title">ECIS Theory – 11:00 AM EST</h4>
         <p class="date">January 6, 2026</p>
         <p>This webinar will provide an overview of the use of impedance (both simple and complex) to detect cell morphological changes. Emphasis will be placed on the use of different AC frequencies to distinguish cell parameters.</p>
         <a class="register" href="https://appliedbiophysics.webex.com/weblink/register/r7068a19f2b1783f4b63cff3ab5e1fda0" target="_blank">Register</a>
       </div>
 
-      <hr>
+      <hr> -->
 
       <div class="webinar">
         <h4 class="webinar-title">Cell Invasion / Extravasation Assays – 11:00 AM EST</h4>
