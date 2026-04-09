763f346957fd316a1858dc658c502aa4f1aba04e
Chris Davis <chris.davis5440@gmail.com>
2026-04-09 15:17:42 -0400

    Created new draft page for the 8wtfa

---
+++ 8wTFADraft.php
@@ -0,0 +1,272 @@
+<?php
+  $title = "8WTFA - 8-well Transfilter Array - Applied BioPhysics";
+  $metaDesc = "The ECIS 8-well Transfilter Array (8wTFA), TEER is collected continously in real-time while the cells remain incubated, making this an essential tool for accurate barrier function quantification.";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name="description" content="<?php echo $metaDesc; ?>">
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Lato:300,400,600,700,900" rel="stylesheet">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
+  <!-- Bootstrap -->
+  <link rel="stylesheet" href="public/css/bootstrap.css" type="text/css">
+  <!-- Custom CSS -->
+  <link rel="stylesheet" href="public/css/style.css">
+  <!-- Favicon -->
+  <link rel="icon" type="image/x-icon" href="public/img/favicon.ico">
+  <title><?php echo $title; ?></title>
+</head>
+
+<body class="d-flex flex-column">
+
+  <?php include_once "includes/navbar.php";?>
+  <?php include_once "includes/logo-sm.php";?>
+
+  <section id="productHeading" class="pb-4 text-center text-md-left">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <h1 class="display-3">ECIS<sup class="registered">®</sup> 8wTFA</h1>
+          <hr class="my-2">
+          <h2>8-well Transfilter Adapter</h2>
+        </div>
+      </div>
+    </div>
+  </section><!-- /productHeading -->
+
+  <section id="productInfo" class="pb-5 text-center text-md-left">
+    <div class="container">
+
+      <div class="row align-items-center mb-4 mb-xl-0">
+        <div class="col-md-7">
+          <p>
+            Traditional methods for measuring transepithelial electrical resistance (TEER) have been limited to single timepoint measurements and require removal of the cells from incubated conditions. With the ECIS 8-well Transfilter Array (8wTFA) that connects to the 16-well array station, TEER is collected continously in real-time while the cells remain incubated, making this an essential tool for accurate TEER quantification.
+          </p>
+        </div>
+        <div class="col-md-5 px-lg-5">
+          <img class='img-fluid d-block mx-auto' src='public/img/8wtfa-1.jpg' alt='8-well Transfilter Array' />
+        </div>
+      </div>
+
+      <div class='pb-3'>
+        <a href="/contactABP.php" class="btn blueBtn mr-2 mb-2" role="button">Request a Quote</a>
+        <a href="/contactABP.php" class="btn ecisDarkBtn mr-2 mb-2" role="button">Talk Applications</a>
+        <a href="/whatIsECIS.php" class="btn ecisDarkBtn mb-2" role="button">What Is ECIS?</a>
+      </div>
+
+      <h3 class="font-weight-bold mt-5 mb-5">Why choose the ECIS<sup>®</sup> 8wTFA?</h3>
+
+      <div class='pb-5'>
+        <div class="row d-flex align-items-start">
+          <div class="col-lg-4">
+            <div class="p-3 border border-dark my-0 mx-0 rounded-xxl">
+              <h5 class="font-weight-bold">Run TEER Continuously</h5>
+              <p class="m-0">Measure the dynamics of barrier formation to maturity in real-time.
+              </p>
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <div class="p-3 border border-dark my-0 mx-0 rounded-xxl mt-3 mt-lg-0">
+              <h5 class="font-weight-bold">Incubated Measurements</h5>
+              <p class="m-0">The 8wTFA conducts TEER measurements while the cells remain inside of the incubator.
+              </p>
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <div class="p-3 border border-dark my-0 mx-0 rounded-xxl mt-3 mt-lg-0">
+              <h5 class="font-weight-bold">Fixed Electrodes</h5>
+              <p class="m-0">No need to worry about variability of measurements from moving electrodes, these are fixed!
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <h3 class="font-weight-bold mt-5 mb-5">Applying ECIS<sup>®</sup> Technology to TEER Assays</h3>
+
+      <div class="row align-items-start mb-4 mb-xl-0">
+        <div class="col-md-7">
+          <p>
+            The ECIS<sup>®</sup> 8-well Transfilter Array (8wTFA) allows researchers to measure transepithelial electrical resistance (TEER) on standard cell culture 24-well inserts using Electric Cell-substrate Impedance Sensing (ECIS<sup>®</sup>). The 8wTFA connects with the ECIS<sup>®</sup> Z-Theta 16-well array station and can monitor TEER continuously in real-time without having to remove the cells from the incubator.
+          </p>
+        </div>
+        <div class="col-md-5 px-lg-5 mt-4 mt-md-0">
+          <img class='img-fluid d-block mx-auto' src='public/img/8wtfaDiagramLabeled.jpg' alt='8wtfa Diagram Labeled' />
+        </div>
+      </div>
+
+      <h3 class="font-weight-bold mt-5 mb-5">Using Resistance <i>and</i> Capacitance with the 8wTFA</h3>
+
+      <div class="row align-items-start mb-4 mb-xl-0">
+        <div class="col-lg-6 col-md-7">
+          <p>
+            ECIS<sup>®</sup> Z-theta measures complex impedance across multiple AC frequencies and separates impedance into series resistance (R) and capacitance (C). For TEER, low frequency series resistance and capacitive reactance are isolated and then converted to an RC parallel circuit providing the real resistance for TEER calculations. High frequency capacitance can also be used with the 8wTFA to determine the point of confluence.
+          </p>
+        </div>
+        <div class="col-lg-6 col-md-5 px-lg-5 mt-4 mt-md-0">
+          <img class='img-fluid d-block mx-auto' src='public/img/8wTFA-D.jpg' alt='8wtfa TEER' />
+        </div>
+      </div>
+
+      <div class="row align-items-start mb-4 mb-xl-0">
+        <div class="col-lg-6 col-md-7">
+          <ul class="list-inline">
+            <li class="ml-3">
+              • <span class="font-weight-bold">Low frequency: </span>Series resistance and capacitive reactance are converted to an RC parallel circuit for correct TEER calculations.
+            </li>
+            <li class="ml-3 my-3">
+              • <span class="font-weight-bold">High frequency: </span>Capacitance is used to determine confluence of cell monolayer.
+            </li>
+            <li class="ml-3">
+              • <span class="font-weight-bold">Same experiment: </span>Use multi-frequency measurements to interpret time of confluence and TEER together.
+            </li>
+          </ul>
+        </div>
+        <div class="col-lg-6 col-md-5 px-lg-5 mt-4 mt-md-0">
+          <img class='img-fluid d-block mx-auto' src='public/img/8wTFA-C.jpg' alt='8wtfa Capacitance' />
+        </div>
+      </div>
+
+      <a href="public/pdf/MeasuringTEERwiththeECIS8wellTransfilterArray.pdf" target="_blank" class="btn ecisDarkBtn mb-5" role="button">Download Application Note</a>
+
+      <h3 class="font-weight-bold mt-5 mt-lg-2 mb-4">Products Related to the 8wTFA</h3>
+
+      <div class="row mt-5 d-flex justify-content-start">
+        <div class="col-md-6">
+          <a class="text-dark link text-decoration-none" href="ztheta.php">
+            <div class="card product-card mb-3 rounded-xxl border-dark">
+              <div class="card-body text-center pb-1">
+                <h4>ECIS<sup>®</sup> Z-Theta w/16-well Array Station</h4>
+              </div>
+              <img class="card-img-top img-fluid w-50 mx-auto d-block" src="public/img/zTheta16.jpg" alt="ECIS zTheta">
+            </div>
+          </a>
+        </div>
+      </div>
+
+      <h3 class="font-weight-bold mt-5 mb-4">Related Applications</h3>
+
+      <div class="mb-5">
+        <a href="/teer.php" class="btn ecisDarkBtn mb-2 mr-1" role="button">TEER</a>
+      </div>
+  </section>
+
+  <?php include_once "includes/footer.php";?>
+
+  <!-- Scroll to top -->
+  <button class="btn btn-outline-dark" id="scrollBtn"><i class="fas fa-angle-up fa-2x"></i></button>
+
+  <!-- Order Info Modal -->
+  <div class="modal fade" id="orderInfo" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h4 class="modal-title">Order Information</h4>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <form id="orderInfoForm" action="/orderInfoProcess.php" method="post">
+            <div class="form-group">
+              <label for="name">Name</label>
+              <input type="text" class="form-control form-control-lg" name="name" id="name" required>
+            </div>
+            <div class="form-group">
+              <label for="company">Institution</label>
+              <input type="text" class="form-control form-control-lg" name="institution" id="institution" required>
+            </div>
+            <div class="form-group">
+              <label for="text">Phone Number</label>
+              <input type="text" class="form-control form-control-lg" name="phone" id="phone" required>
+            </div>
+            <div class="form-group">
+              <label for="email">Email Address</label>
+              <input type="email" class="form-control form-control-lg" name="email" id="email" required>
+            </div>
+            <div class="form-group">
+              <label for="system">System Interested In</label>
+              <input type="text" class="form-control form-control-lg" name="system" id="system" required>
+            </div>
+            <div class="form-group d-none">
+              <!-- Honey -->
+              <input type="text" class="form-control form-control-lg" name="fax" id="fax">
+            </div>
+            <div class="form-group mt-4">
+              <label class="d-block">Would you like to Demo or Purchase?</label>
+              <div class="form-check form-check-inline">
+                <input class="form-check-input demoOrPurchase" type="radio" name="demoOrPurchase" id="demoOrPurchaseDemo" value="Demo" checked>
+                <label class="form-check-label" for="demoOrPurchaseDemo">Demo</label>
+              </div>
+              <div class="form-check form-check-inline">
+                <input class="form-check-input demoOrPurchase" type="radio" name="demoOrPurchase" id="demoOrPurchasePurchase" value="Purchase">
+                <label class="form-check-label" for="demoOrPurchasePurchase">Purchase</label>
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="comments">Comments</label>
+              <textarea class="form-control form-control-lg" name="comments" id="comments"></textarea>
+            </div>
+
+            <div class="row">
+              <div class="col d-flex flex-column">
+                <button id="orderInfoBtn" class="btn ecisBtn btn-lg submit-btn btn-block mt-3 ml-auto">Submit</button>
+                <i class="fas fa-spinner fa-2x fa-spin mt-4 mx-auto"></i>
+                <!--Initially hidden in the css-->
+                <span class="submitMessage text-center mt-4 mb-3 mx-auto">Thank you, your request has been sent</span>
+                <!--Initially hidden in the css-->
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <script src="public/js/jquery.min.js"></script>
+  <script src="public/js/bootstrap.bundle.min.js"></script>
+  <script src="public/js/util.js"></script>
+  <script src="public/js/smooth-scroll.min.js"></script>
+  <script src="public/js/formsHandling.js"></script>
+  <script src="public/js/scripts.js"></script>
+
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-108411540-1"></script>
+  <script>
+  window.dataLayer = window.dataLayer || [];
+
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+  gtag('js', new Date());
+
+  gtag('config', 'UA-108411540-1');
+  </script>
+
+  <!-- Engagebay -->
+  <script type="text/javascript">
+  var EhAPI = EhAPI || {};
+  EhAPI.after_load = function() {
+    EhAPI.set_account('lj2ng4pb8ricu2piqdirfbrv61', 'biophysics');
+    EhAPI.execute('rules');
+  };
+  (function(d, s, f) {
+    var sc = document.createElement(s);
+    sc.type = 'text/javascript';
+    sc.async = true;
+    sc.src = f;
+    var m = document.getElementsByTagName(s)[0];
+    m.parentNode.insertBefore(sc, m);
+  })(document, 'script', '//d2p078bqz5urf7.cloudfront.net/jsapi/ehform.js');
+  </script>
+
+</body>
+
+</html>
